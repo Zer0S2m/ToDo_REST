fc619f3d554953fa02d644617d7b723d6832c73e
Alekandr-zero <zeroday9898@gmail.com>
2022-01-22 15:36:11 +0300

    fixes

--- services/backend/create_db.py
+++ services/backend/create_db.py
@@ -1,6 +1,23 @@
+import asyncio
+import shutil
+import os
+
 from models.models import Base
 from models.models import engine
 
+from config.config import BD_NAME
+
+
+async def main():
+	async with engine.begin() as conn:
+		await conn.run_sync(Base.metadata.create_all)
+
+	source_path = f"{BD_NAME}.db"
+
+	if os.path.exists(source_path):
+		os.mkdir("db")
+		shutil.move(source_path, f"db\{source_path}")
+
 
 if __name__ == '__main__':
-	Base.metadata.create_all(engine)
+	asyncio.run(main())
