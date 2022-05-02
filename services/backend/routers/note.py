1278414e390f022817e690c2d5709832cee49655
Aleksandr <77345140+Zer0S2m@users.noreply.github.com>
2022-05-02 12:38:43 +0300

    Обновление запросов в БД (#9)
    * запросы ДБ в виде класса у модели <Project>, <Part>, <Comment>
    
    * запросы ДБ в виде класса у модели <Category>
    
    * запросы ДБ в виде класса у модели <Note>

--- services/backend/routers/note.py
+++ services/backend/routers/note.py
@@ -14,10 +14,10 @@ from schemas.note import (
 
 from models import Note
 
+from utils.db.category import ServiceDBCategory
+
 from utils.db.note import (
-	get_notes_db, get_note_db, delete_note_db,
-	edit_note_db, craete_note_db, set_file_note,
-	get_file, complete_note_db
+	set_file_note, get_file, ServiceDBNote
 )
 from utils.users import get_current_user
 from utils.common import (
@@ -35,9 +35,9 @@ router = APIRouter(
 
 async def check_is_note_in_db(
 	note_id: int,
-	current_user: UserInDB
+	service: ServiceDBNote
 ) -> Note:
-	note = await get_note_db(note_id, current_user)
+	note = await service.fetch_one(note_id)
 	if not note:
 		raise HTTPException(status_code = 404, detail = "Note not found")
 	return note
@@ -48,13 +48,13 @@ async def check_is_note_in_db(
 	response_model = List[NoteSchema],
 	response_model_exclude_unset = True
 )
-async def list_notes(
+async def get_notes(
 	slug_project: str,
-	current_user: UserInDB = Depends(get_current_user)
+	service: ServiceDBNote = Depends()
 ) -> List[dict]:
 	notes = []
 
-	notes_db = await get_notes_db(current_user, slug_project)
+	notes_db = await service.fetch_all(slug_project)
 	for note in notes_db:
 		note_dict = set_category_note(note = note)
 		note_dict = set_file_name_note(note = note, note_dict = note_dict)
@@ -71,9 +71,9 @@ async def list_notes(
 async def get_note(
 	slug_project: str,
 	note_id: int,
-	current_user: UserInDB = Depends(get_current_user)
+	service: ServiceDBNote = Depends()
 ) -> dict:
-	note = await check_is_note_in_db(note_id, current_user)
+	note = await check_is_note_in_db(note_id, service)
 	note_dict = set_category_note(note = note)
 	note_dict = set_file_name_note(note = note, note_dict = note_dict)
 
@@ -84,10 +84,10 @@ async def get_note(
 async def delete_note(
 	slug_project: str,
 	note: NoteDeleted,
-	current_user: UserInDB = Depends(get_current_user)
+	service: ServiceDBNote = Depends()
 ):
-	await check_is_note_in_db(note.id, current_user)
-	await delete_note_db(note, current_user)
+	await check_is_note_in_db(note.id, service)
+	await service.delete(note)
 
 
 @router.put(
@@ -98,15 +98,15 @@ async def edit_note(
 	slug_project: str,
 	note: NoteEdit = Depends(),
 	file: UploadFile = File(None),
-	current_user: UserInDB = Depends(get_current_user)
+	service: ServiceDBNote = Depends()
 )-> dict:
-	await check_is_note_in_db(note.id, current_user)
+	await check_is_note_in_db(note.id, service)
 
 	data_file = {}
 	if file:
-		data_file = await set_file_note(file, current_user)
+		data_file = await set_file_note(file, service.current_user)
 
-	note = await edit_note_db(note, data_file, current_user)
+	note = await service.edit(note, data_file)
 	note_dict = note.as_dict
 	note_dict = set_category_note(note = note)
 
@@ -119,10 +119,10 @@ async def edit_note(
 async def complete_note(
 	slug_project: str,
 	note: NoteComplete,
-	current_user: UserInDB = Depends(get_current_user)
+	service: ServiceDBNote = Depends()
 ):
-	await check_is_note_in_db(note.id, current_user)
-	await complete_note_db(note, current_user)
+	await check_is_note_in_db(note.id, service)
+	await service.complete(note)
 
 
 @router.post(
@@ -134,17 +134,18 @@ async def create_note(
 	slug_project: str,
 	note: NoteCreate = Depends(),
 	file: UploadFile = File(None),
-	current_user: UserInDB = Depends(get_current_user)
+	service_category: ServiceDBCategory = Depends(),
+	service_note: ServiceDBNote = Depends()
 ) -> dict:
 	file_id = 0
 	file_name = None
 
 	if file:
-		data_file = await set_file_note(file, current_user)
+		data_file = await set_file_note(file, service_note.current_user)
 		file_id = data_file["file_id"]
 		file_name = data_file["file_name"]
 
-	data = await craete_note_db(note = note, file_id = file_id, current_user = current_user)
+	data = await service_note.create(note = note, file_id = file_id, service_category = service_category)
 	note_dict = data["new_note"].as_dict
 	note_dict["file_name"] = file_name
 
